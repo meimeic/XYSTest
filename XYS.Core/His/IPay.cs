7aab1519f7a7c32d04a0ca261560b9dbf55b56f4
meimeic <981203234@qq.com>
2014-12-26 17:40:02 +0800

    ss

--- XYS.Core/His/IPay.cs
+++ XYS.Core/His/IPay.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace XYS.Core.His
 {
-    interface IRegist
+    interface IPay
     {
     }
 }
---
+++ XYS.Core/His/IRegister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XYS.Core.His
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRegister
+    {
+    }
+}
