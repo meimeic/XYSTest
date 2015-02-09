1b4762a02db8cf0cc78794c70f6de4580b844c8c
meimeic <981203234@qq.com>
2015-02-09 16:48:09 +0800

    rr

---
+++ XYS.His.Security/Self/SourceCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XYS.His.Security.Self
+{
+
+    public enum CardType
+    {
+        IDCard=1, //身份证
+        BankCard, //银行卡
+        QRCard,  //二维码卡片
+        InsCard,  //医保卡
+        Other    //其他
+    }
+    public class SourceCard
+    {
+
+    }
+}
